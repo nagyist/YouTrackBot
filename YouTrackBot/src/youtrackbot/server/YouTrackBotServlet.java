ca94abffadc2a36b9803206f8b62cb3fd667a90a
jan0sch <jan0sch@gmx.net>
2010-07-16 19:13:58 +0000

    YTB-1: Prototyp
    git-svn-id: https://vcs.wegtam.com/svn/private/youtrackbot/trunk@37 1b18abfc-8af7-4d26-9701-7deff39b8ec0

--- YouTrackBot/src/youtrackbot/server/YouTrackBotServlet.java
+++ YouTrackBot/src/youtrackbot/server/YouTrackBotServlet.java
@@ -1,37 +1,66 @@
 package youtrackbot.server;
 
-import com.google.wave.api.*;
+import com.google.wave.api.AbstractRobot;
+import com.google.wave.api.Blip;
+import com.google.wave.api.Context;
+import com.google.wave.api.Element;
 import com.google.wave.api.event.*;
+import org.jetbrains.annotations.NonNls;
+
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import java.util.logging.Logger;
 
 /**
  * @author Jens Jahnke <jan0sch@gmx.net>
  * @version $Id$
  */
 public class YouTrackBotServlet extends AbstractRobot {
-  @Override
-  protected String getRobotName() {
-    return "YouTrackBot";
-  }
+    @NonNls
+    private final static String robotId = "youtrackbot@appspot.com";
+    @NonNls
+    private final static String issueRegEx = "#(\\w+-\\d+)";
+    private final static Pattern p = Pattern.compile(issueRegEx, Pattern.UNICODE_CASE | Pattern.CASE_INSENSITIVE);
+    // This is for logging errors to the appengine logs.
+    private static final Logger log = Logger.getLogger(YouTrackBotServlet.class.getName());
+    //TODO Remove these and make them configurable per wave.
+    private static String trackerUrl = "https://dev.wegtam.com";
+    private final static String issuePath = "issue";
 
-  @Override
-  protected String getRobotAvatarUrl() {
-    return "http://www.jetbrains.com/img/icons/YT_big.gif";
-  }
-
-  @Override
-  protected String getRobotProfilePageUrl() {
-    return "http://youtrackbot.appspot.com/";
-  }
-
-  @Override
-  public void onWaveletSelfAdded(WaveletSelfAddedEvent event) {
-    Blip blip = event.getWavelet().reply("\nHi everybody!");
-  }
-
-  @Override
-  public void onWaveletParticipantsChanged(WaveletParticipantsChangedEvent event) {
-    for (String newParticipant: event.getParticipantsAdded()) {
-      Blip blip = event.getWavelet().reply("\nHi : " + newParticipant);
+    @Override
+    protected String getRobotName() {
+        return "YouTrackBot";
     }
-  }
+
+    @Override
+    protected String getRobotAvatarUrl() {
+        return "http://www.jetbrains.com/img/icons/YT_big.gif";
+    }
+
+    @Override
+    protected String getRobotProfilePageUrl() {
+        return "http://youtrackbot.appspot.com/";
+    }
+
+    @Override
+    @EventHandler.Capability(contexts = {Context.SELF}, filter = issueRegEx)
+    public void onBlipSubmitted(BlipSubmittedEvent event) {
+        if (!event.getModifiedBy().equalsIgnoreCase(robotId)) {
+            StringBuilder issueUrl;
+            Blip blip = event.getBlip();
+            Pattern p = Pattern.compile(issueRegEx, Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+            Matcher m = p.matcher(blip.serialize().getContent());
+            while (m.find()) {
+                issueUrl = new StringBuilder(trackerUrl);
+                blip.first(m.group()).replace(issueUrl.append("/").append(issuePath).append("/").append(m.group(1)).toString());
+            } // while
+        }
+    }
+
+    @Override
+    public void onWaveletSelfAdded(WaveletSelfAddedEvent event) {
+        event.getWavelet().reply("\nHi everybody!");
+    }
 }
