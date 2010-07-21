5a90fdb0000c2cefe06f4e9a67db5d78039123b7
jan0sch <jan0sch@gmx.net>
2010-07-21 17:31:46 +0000

    YTB-2: Konfiguration der URL
    git-svn-id: https://vcs.wegtam.com/svn/private/youtrackbot/trunk@48 1b18abfc-8af7-4d26-9701-7deff39b8ec0

--- YouTrackBot/src/youtrackbot/server/YouTrackBotServlet.java
+++ YouTrackBot/src/youtrackbot/server/YouTrackBotServlet.java
@@ -1,17 +1,13 @@
 package youtrackbot.server;
 
 import com.google.wave.api.*;
-import com.google.wave.api.event.BlipSubmittedEvent;
-import com.google.wave.api.event.EventHandler;
-import com.google.wave.api.event.FormButtonClickedEvent;
-import com.google.wave.api.event.WaveletSelfAddedEvent;
+import com.google.wave.api.event.*;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.jdo.JDOObjectNotFoundException;
 import javax.jdo.PersistenceManager;
-import javax.jdo.Query;
-import java.util.List;
 import java.util.SortedMap;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
@@ -55,9 +51,13 @@ public class YouTrackBotServlet extends AbstractRobot {
     public void onBlipSubmitted(BlipSubmittedEvent event) {
         if (!event.getModifiedBy().equalsIgnoreCase(robotId)) {
             String waveId = event.getWavelet().getWaveId().toString();
-            YouTrackInstance instance = loadYouTrackInstance(waveId);
-            if (instance == null) {
+            YouTrackInstance instance;
+            try {
+                instance = loadYouTrackInstance(waveId);
+            } catch (JDOObjectNotFoundException e) {
                 instance = new YouTrackInstance();
+                instance.setId(waveId);
+                saveYouTrackInstance(instance);
             }
             StringBuilder issueUrl;
             Blip blip = event.getBlip();
@@ -74,25 +74,24 @@ public class YouTrackBotServlet extends AbstractRobot {
     @Capability(contexts = Context.SELF)
     public void onFormButtonClicked(FormButtonClickedEvent event) {
         if (event.getButtonName().equals(submitButtonName)) {
-            log.info("Submit button was clicked.");
             String waveId = event.getWavelet().getWaveId().toString();
-            YouTrackInstance instance = loadYouTrackInstance(waveId);
-            assert instance != null;
+            YouTrackInstance instance;
+            try {
+                instance = loadYouTrackInstance(waveId);
+            } catch (JDOObjectNotFoundException e) {
+                instance = new YouTrackInstance();
+                instance.setId(waveId);
+                saveYouTrackInstance(instance);
+            }
             SortedMap<Integer, Element> elements = event.getBlip().getElements();
-            log.info("Got " + elements.size() + " elements.");
             for (int key : elements.keySet()) {
                 Element e = elements.get(key);
-                log.info("Checking element " + e);
                 if (e.isFormElement() && e.getType() == ElementType.INPUT && e.getProperty("name").equals(trackerUrlInputFieldName)) {
                     instance.setTrackerUrl(e.getProperty("value"));
-                    saveYouTrackInstance(instance);
-                    log.info("Value : " + e.getProperty("value"));
+                    log.info("Value : " + e.getProperty("value") + " Instance : " + instance.getTrackerUrl());
                     break; // We don't need to go any further.
                 }
             } // for
-        }
-        else {
-            log.warning("Not our button!");
         }
     }
 
@@ -100,16 +99,16 @@ public class YouTrackBotServlet extends AbstractRobot {
     public void onWaveletSelfAdded(WaveletSelfAddedEvent event) {
         String waveId = event.getWavelet().getWaveId().toString();
         log.info("Robot was added to wave " + waveId + ".");
-        YouTrackInstance instance = loadYouTrackInstance(waveId);
-        if (instance != null) {
-            log.info("Found saved instance for this wave (" + instance.getTrackerUrl() + ").");
-        } else {
-            log.info("No saved instance found for this wave.");
+        YouTrackInstance instance;
+        try {
+            instance = loadYouTrackInstance(waveId);
+        } catch (JDOObjectNotFoundException e) {
             instance = new YouTrackInstance();
             instance.setId(waveId);
             saveYouTrackInstance(instance);
         }
         Blip helloWorld = event.getWavelet().reply("\nHi everybody! Please use #XX-NN to link to existing tickets where XX is the shortcut for your project and NN the ticket number.\n\n");
+        helloWorld.appendMarkup("<p><em>To change the url for your YouTrack instance please enter it in the form below and click on the save button.</em></p>");
         helloWorld.appendMarkup("<strong>Tracker URL</strong>");
         Element element = new Element(ElementType.INPUT);
         element.setProperty("name", trackerUrlInputFieldName);
@@ -121,54 +120,48 @@ public class YouTrackBotServlet extends AbstractRobot {
         element.setProperty("name", submitButtonName);
         element.setProperty("value", "Save");
         helloWorld.append(element);
-        helloWorld.append("\n\n");
+        helloWorld.append("\n");
     }
 
-//    @Override
-//    public void onWaveletSelfRemoved(WaveletSelfRemovedEvent event) {
-//        String waveId = event.getWavelet().getWaveId().toString();
-//        log.info("Robot was removed from wave " + waveId + ".");
-//        YouTrackInstance instance = loadYouTrackInstance(waveId);
-//        if (instance != null) {
-//            log.info("Found saved instance for this wave (" + instance.getTrackerUrl() + ").");
-//            deleteYouTrackInstance(instance);
-//        }
-//    }
-//
-//    /**
-//     * Delete a YouTrack instance from the database.
-//     *
-//     * @param instance The instance to delete.
-//     */
-//    private void deleteYouTrackInstance(@NotNull YouTrackInstance instance) {
-//        PersistenceManager pm = YouTrackBotPMF.getPmfInstance().getPersistenceManager();
-//        try {
-//            pm.deletePersistent(pm.getObjectById(instance.getClass(), instance.getId()));
-//            log.info("Deleted saved instance from database.");
-//        } finally {
-//            pm.close();
-//        }
-//    }
+    @Override
+    public void onWaveletSelfRemoved(WaveletSelfRemovedEvent event) {
+        String waveId = event.getWavelet().getWaveId().toString();
+        log.info("Robot was removed from wave " + waveId + ".");
+        YouTrackInstance instance;
+        try {
+            instance = loadYouTrackInstance(waveId);
+            deleteYouTrackInstance(instance);
+        } catch (JDOObjectNotFoundException e) {
+            log.info("No saved instance found for this wave.");
+        }
+    }
+
+    /**
+     * Delete a YouTrack instance from the database.
+     *
+     * @param instance The instance to delete.
+     */
+    private void deleteYouTrackInstance(@NotNull YouTrackInstance instance) {
+        PersistenceManager pm = YouTrackBotPMF.getPmfInstance().getPersistenceManager();
+        try {
+            pm.deletePersistent(pm.getObjectById(instance.getClass(), instance.getId()));
+            log.info("Deleted saved instance from database.");
+        } finally {
+            pm.close();
+        }
+    }
 
     /**
      * Loads a stored YouTrack instance from the database.
      *
      * @param waveId The wave id which is the id of the YouTrack instance.
      * @return A YouTrack instance or {@code null}.
+     * @throws javax.jdo.JDOObjectNotFoundException An exception is thrown if the object is not found in the database.
      */
     @Nullable
-    private YouTrackInstance loadYouTrackInstance(String waveId) {
-        YouTrackInstance instance = null;
+    private YouTrackInstance loadYouTrackInstance(String waveId) throws JDOObjectNotFoundException {
         PersistenceManager pm = YouTrackBotPMF.getPmfInstance().getPersistenceManager();
-        Query query = pm.newQuery(YouTrackInstance.class);
-        query.setFilter("id == idParameter");
-        query.declareParameters("String idParameter");
-        List<YouTrackInstance> results = (List<YouTrackInstance>) query.execute(waveId);
-        if (results.size() == 1) {
-            instance = results.get(0);
-        }
-        pm.close();
-        return instance;
+        return pm.getObjectById(YouTrackInstance.class, waveId);
     }
 
     /**
